34bb8007d0d0207222cf41e2aa16bde1107d6329
Eduar <eduar@users.noreply.github.com>
2026-06-21 20:16:38 -0400

    PWA: manifest.json, service worker, instalable desde el navegador

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'preguntas-nsdc-v1';
+const ASSETS = [
+  '/',
+  '/remixed-0d6392aa.html',
+  '/manifest.json',
+  '/images/logochico.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
